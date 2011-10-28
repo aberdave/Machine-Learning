777e67c38e7ada43674aefb815ce442e94cf9aa3
David Abercrombie <david.abercrombie@tapjoy.com>
2011-10-27 22:07:17 -0700

    got dendograms for both scaled and raw
    I have no idea how to interpret the output

--- ml102/hw1/abercrombie-ml102-hw1-p2.R
+++ ml102/hw1/abercrombie-ml102-hw1-p2.R
@@ -44,6 +44,7 @@ control.chart.raw.km <- kmeans(
 control.chart.raw.diana <- diana(
   x=control.chart.raw.df, 
   diss=FALSE,
+  stand=FALSE,
   metric = "euclidean"
 )
 
@@ -63,155 +64,28 @@ plot(
 
 
 #####################################################################
-# Step 3. Exract and prepare cluster assignments for review
+# Step 3. Do diana on scaled dataset
 
-# Intercative look at structure of cluster element
-# It is a vector of 600 integers
-#
-str(control.chart.raw.km$cluster)
-
-# We know that the data had six types of control charts, with 100 examples 
-# of each, in contiguous chunks of rows
-# See http://archive.ics.uci.edu/ml/databases/synthetic_control/synthetic_control.data.html
-# So lets create some factors and attach them to cluster assingment as a data frame
-#
-control.chart.type.factors <- as.factor(
-  c(
-    rep('1. Normal',100),
-    rep('2. Cyclicl',100),
-    rep('3. Increasing trend',100),
-    rep('4. Decreasing trend',100),
-    rep('5. Upward shift',100),
-    rep('6. Downward shift',100)
-  )
-)
-str(control.chart.type.factors)
-
-control.chart.raw.clusters.df <- data.frame(
-  actual.type=control.chart.type.factors,
-  modeled.cluster=control.chart.raw.km$cluster
-)
-str(control.chart.raw.clusters.df)
-
-
-#####################################################################
-# Step 4. Use a lattice of histograms to review
-
-# Lattice graphics includes methods to put many histograms on 
-# the same chart. It is installed by default, but not loaded
-# by default.
-#
-require('lattice')
-
-
-# The first argument is a "model formula" 
-# in "Wilkinon-Rogers (1973)" notation (see Venables-Ripley chap. 6)
-# Think of it as saying that the modeled.cluster values depend on the
-# control.chart.raw.clusters.
-# The third argument. layout, and is poorly documented. I learned this
-# trick from "R in a Nutshell" Figure 15-12.
-#
-histogram(
-  ~modeled.cluster|actual.type, 
-  data=control.chart.raw.clusters.df,
-  layout=c(1,6)
+# Use diss=FALSE since these observations
+# now we standardize
+control.chart.raw.diana <- diana(
+  x=control.chart.raw.df, 
+  diss=FALSE,
+  metric = "euclidean"
 )
 
-# Reviewing the chart, I am amazed at how well the clustering did work.
-# The normal charts are all in a cluste of their own. The clyclic charts 
-# are off by themselves, but split into two sub groups. Cyclics are clearly
-# distinguished from decreasing trend, but confused with upward shift.
-# Interesting, far from arbitrary, but not compelling.
-#
-
-#####################################################################
-# Step 5. Since data are from time series, see if deltas work better
-
-# 5A: convert time series data frame to matrix for simpler 
-#     math and indexing notation 
-#
-control.chart.raw.m <- as.matrix(control.chart.raw.df)
-
-# Create a data frame that has the deltas betwen one observation
-# and the next:
-#
-# 5B. Initialize the first column of data frame by subtracting the 
-# the matrix first column from it's second. I use drop=FALSE to
-# preserve the column-like nature of the subtraction.
-#
-# Inspired by these recepies from the R Cookbook:
-#    5.17 Selecting One Row or Column from a Matrix
-#    5.29 Combining Two Data Frames
-#
-control.chart.deltas.df <- data.frame(
-  control.chart.raw.m[,2,drop=FALSE] - control.chart.raw.m[,1,drop=FALSE]
-)
-
-
-# 5C. Iterate through the remaining matrix columns,
-# appending fresh delta column to existing data frame
-#
-for ( i in 3:ncol(control.chart.raw.m) ) {
-  # calculate the next column of deltas
-  # by subtracting column 1-2 from column i
-  new.column <- data.frame(
-    control.chart.raw.m[,i,drop=FALSE] - control.chart.raw.m[,i-1,drop=FALSE]
-  )
-  # combine the new column with the existing
-  # data frame, column-wise
-  control.chart.deltas.df <-cbind(
-    control.chart.deltas.df,
-    new.column
-  )
-}
-# expecting 600 59
-dim(control.chart.deltas.df)
-
-#####################################################################
-# Step 6. Do k-means on delta dataset
-
-control.chart.deltas.km <- kmeans(
-  x=control.chart.deltas.df,
-  centers=6,
-  iter.max=20,
-  nstart=10
-)
-
+str(control.chart.raw.diana)
 # Interactive look at return value structure
 #
-summary(control.chart.deltas.km)
+summary(control.chart.raw.diana)
 
-#####################################################################
-# Step 7. Use a lattice of histograms to review delta clusters
-
-
-control.chart.deltas.clusters.df <- data.frame(
-  actual.type=control.chart.type.factors,
-  modeled.cluster=control.chart.deltas.km$cluster
-)
-str(control.chart.deltas.clusters.df)
-
-
-# Lattice graphics includes methods to put many histograms on 
-# the same chart. It is installed by default, but not loaded
-# by default.
-#
-require('lattice')
-
-
-# The first argument is a "model formula" 
-# in "Wilkinon-Rogers (1973)" notation (see Venables-Ripley chap. 6)
-# Think of it as saying that the modeled.cluster values depend on the
-# control.chart.raw.clusters.
-# The third argument. layout, and is poorly documented. I learned this
-# trick from "R in a Nutshell" Figure 15-12.
-#
-histogram(
-  ~modeled.cluster|actual.type, 
-  data=control.chart.deltas.clusters.df,
-  layout=c(1,6)
+plot(
+  x=control.chart.raw.diana,
+  ask=FALSE,
+  which.plots=2
 )
 
-# This did a horrible job!!!! Really bad!!!
 
+
+# I have no idea how to interpret these charts
 
